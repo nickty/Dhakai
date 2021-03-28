ed5a090906659b32c4786cf5cca92f1066dff6f9
Mizanur Rahman <nickty.86@gmail.com>
2021-03-28 07:16:13 +0600

    task completed

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -5,7 +5,7 @@ const PasswordInput = () => {
     return (
         <Form.Group as={Col} controlId="formGridPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" />
         </Form.Group>
     )
 }
